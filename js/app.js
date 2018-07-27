f5e8fdab00fdc3c0477507faced2c31b6c85d6c4
Sarang-Khajuria <srng.khajuria1@gmail.com>
2018-07-28 02:20:35 +0530

    Dynamically creating grid for the puzzle

--- js/app.js
+++ js/app.js
@@ -2,10 +2,14 @@ window.onload = function() {
     let elements = document.getElementsByClassName('innerelems');
     let elemArray = [];
     let i=0;
+    let numberOfRows = 4;
+    let numberOfColumns = 5;
+    createGrid(numberOfRows,numberOfColumns);
+    let numberOfElements = numberOfRows * numberOfColumns
     let elemPos = elements.length-1;
     var empty = elements[elemPos];
     while(i<elements.length-1){
-        let rand = Math.floor(Math.random()* (16 - 1)+1);
+        let rand = Math.floor(Math.random()* (numberOfElements - 1)+1);
         if(elemArray.indexOf(rand) == -1){
             elemArray.push(rand);
             elements[i].innerHTML = rand;
@@ -16,7 +20,7 @@ window.onload = function() {
     window.addEventListener('keyup',function(event){
         let keycode = event.which || event.keycode;
         if(keycode == 37){
-            if(elemPos %4 !=0){
+            if(elemPos %numberOfColumns !=0){
                 let element1 = empty.cloneNode(true);
                 let element2 = elements[elemPos-1].cloneNode(true);
                 elements[elemPos-1].parentNode.replaceChild(element1,elements[elemPos-1]);
@@ -29,18 +33,18 @@ window.onload = function() {
             
         }
         if(keycode == 38){
-            if(elemPos >= 4){
+            if(elemPos >= numberOfColumns){
                 let element1 = empty.cloneNode(true);
                 let element2 = elements[elemPos-4].cloneNode(true);
-                elements[elemPos-4].parentNode.replaceChild(element1,elements[elemPos-4]);
+                elements[elemPos-numberOfColumns].parentNode.replaceChild(element1,elements[elemPos-numberOfColumns]);
                 empty.parentNode.replaceChild(element2,empty);
-                swapElements(elemArray,elemPos,elemPos-4);
-                elemPos = elemPos-4;
+                swapElements(elemArray,elemPos,elemPos-numberOfColumns);
+                elemPos = elemPos-numberOfColumns;
                 empty=elements[elemPos];
             }
         }
         if(keycode == 39){
-            if((elemPos+1) %4 !=0){
+            if((elemPos+1) %numberOfColumns !=0){
                 let element1 = empty.cloneNode(true);
                 let element2 = elements[elemPos+1].cloneNode(true);
                 elements[elemPos+1].parentNode.replaceChild(element1,elements[elemPos+1]);
@@ -51,13 +55,13 @@ window.onload = function() {
             }
         }
         if(keycode == 40){
-            if(elements.length-4 > elemPos){
+            if(elements.length-numberOfColumns > elemPos){
                 let element1 = empty.cloneNode(true);
-                let element2 = elements[elemPos+4].cloneNode(true);
-                elements[elemPos+4].parentNode.replaceChild(element1,elements[elemPos+4]);
+                let element2 = elements[elemPos+numberOfColumns].cloneNode(true);
+                elements[elemPos+numberOfColumns].parentNode.replaceChild(element1,elements[elemPos+numberOfColumns]);
                 empty.parentNode.replaceChild(element2,empty);
-                swapElements(elemArray,elemPos,elemPos+4);
-                elemPos = elemPos+4;
+                swapElements(elemArray,elemPos,elemPos+numberOfColumns);
+                elemPos = elemPos+numberOfColumns;
                 empty=elements[elemPos];
             }
         }
@@ -65,6 +69,30 @@ window.onload = function() {
   };
 
   
+
+ 
+
+
+  function createGrid(numberOfRows,numberOfColumns){
+    let slidingRow = document.getElementsByClassName('sliding-row')[0];
+    slidingRow.style.height = (numberOfRows * 75) + 'px';
+    for(let index = 0;index<numberOfRows;index++){
+        let element = document.createElement('div');
+        element.setAttribute('class','rows');
+        slidingRow.appendChild(element);
+    }
+    let rows = document.getElementsByClassName('rows');
+    let width =  100/numberOfColumns;
+    width = width + '%';
+    for(let rowIndex = 0;rowIndex<rows.length;rowIndex++){
+        for(let index = 0;index < numberOfColumns;index++){
+            let element = document.createElement('div');
+            element.setAttribute('class','innerelems');
+            element.style.flexBasis = width;
+            rows[rowIndex].appendChild(element);
+        }
+    }   
+  } 
 
   
   
